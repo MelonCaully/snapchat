97cd3a7033c4cfc68fca99d81903a8ddbc6059d9
MelonCaully <maloneingham@gmail.com>
2023-02-02 09:52:02 -0500

    Footer & Nav Made, Code Transported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooternavComponent } from './footernav/footernav.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooternavComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
