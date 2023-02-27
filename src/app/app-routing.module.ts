83369bb14e44fac7f38efa3403190097373fae39
MelonCaully <maloneingham@gmail.com>
2023-02-27 15:37:31 -0500

    All Layouts Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstLayoutComponent } from './Layout/first-layout/first-layout.component';
+import { FourthLayoutComponent } from './Layout/fourth-layout/fourth-layout.component';
 import { HomeLayoutComponent } from './Layout/home-layout/home-layout.component';
 import { SecondLayoutComponent } from './Layout/second-layout/second-layout.component';
+import { ThirdLayoutComponent } from './Layout/third-layout/third-layout.component';
 
 const routes: Routes = [
   {
@@ -9,9 +12,21 @@ const routes: Routes = [
     component: HomeLayoutComponent
   },
   {
+    path: 'FirstLayout',
+    component: FirstLayoutComponent
+  },
+  {
     path: 'SecondLayout',
     component: SecondLayoutComponent
   },
+  {
+    path: 'ThirdLayout',
+    component: ThirdLayoutComponent
+  },
+  {
+    path: 'FourthLayout',
+    component: FourthLayoutComponent
+  }
 ];
 
 @NgModule({
