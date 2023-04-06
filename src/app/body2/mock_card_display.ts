e6e407a39a40fbeeb5e3314cd5a38457bc20e5eb
MelonCaully <maloneingham@gmail.com>
2023-04-06 15:27:32 -0400

    Mock List Created For Cards

---
+++ src/app/body2/mock_card_display.ts
@@ -0,0 +1,26 @@
+export const mock_card_display = [
+    {
+        img: "assets/IOS.jpg",
+        title: "IOS",
+        button: "Download",
+        link: "https://apps.apple.com/app/apple-store/id447188370"
+    },
+    {
+        img: "assets/android.jpg",
+        title: "Android",
+        button: "Download",
+        link: "https://play.google.com/store/apps/details?id=com.snapchat.android&referrer=download_page"
+    },
+    {
+        img: "assets/Labtop Snap.png",
+        title: "Desktop",
+        button: "Log in to Chat",
+        link: "SecondLayout"
+    },
+    {
+        img: "",
+        title: "Chat on the web!",
+        button: "Log in to Chat",
+        link: "SecondLayout"
+    }
+];
