22052f35873aaeaeb5cec71d44bcc57b6493f26e
MelonCaully <maloneingham@gmail.com>
2023-04-06 17:26:44 -0400

    Checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { CardDisplayModel } from './body2/card_display.model';
-import { mock_card_list } from './body2/mock_card_list';
+import { CardDisplayModel } from './card_display.model';
+import { mock_card_list } from './mock_card_list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/body2/body2.component.ts
+++ src/app/body2/body2.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CardDisplayModel } from '../card_display.model';
+import { mock_card_list } from '../mock_card_list';
 
 @Component({
   selector: 'app-body2',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./body2.component.css']
 })
 export class Body2Component {
+  cards: CardDisplayModel [] = [];
 
+  constructor(){
+    for(var card of mock_card_list){
+      console.log(card);
+      this.cards.push(card);
+    }
+  }
 }
