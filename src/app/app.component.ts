e33187990e5e44b074d57519e6fbae3131310d94
MelonCaully <maloneingham@gmail.com>
2023-04-06 17:19:43 -0400

    Console Log Created for Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CardDisplayModel } from './body2/card_display.model';
+import { mock_card_list } from './body2/mock_card_list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'snapchat';
+  cards: CardDisplayModel [] = [];
+
+  constructor(){
+    for(var card of mock_card_list){
+      console.log(card);
+      this.cards.push(card);
+    }
+  }
 }
