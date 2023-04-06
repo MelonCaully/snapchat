4ba08aa3e22ccbcf2980d15ceb51afb1fd63437f
MelonCaully <maloneingham@gmail.com>
2023-04-06 16:30:12 -0400

    checkpoint

---
+++ src/app/body2/card_display.model.ts
@@ -0,0 +1,13 @@
+export class CardDisplayModel {
+    img: string;
+    title: string;
+    button: string;
+    link: string;
+
+    constructor(img: string, title: string, button: string, link: string) {
+        this.img = img;
+        this.title = title;
+        this.button = button;
+        this.link = link;
+    }
+}
