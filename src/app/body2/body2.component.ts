46e1c8c1ac4158bffc90fb1bcd7a7afa2ab121a6
MelonCaully <maloneingham@gmail.com>
2023-04-06 20:11:32 -0400

    Files Moved Into body 2 component

--- src/app/body2/body2.component.ts
+++ src/app/body2/body2.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductModel } from '../product.model';
-import { mock_card_list } from '../mock_card_list';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductModel } from './product.model';
+import { mock_card_list } from './mock_card_list';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-body2',
@@ -10,13 +11,17 @@ import { mock_card_list } from '../mock_card_list';
 export class Body2Component implements OnInit{
   cards: ProductModel [] = [];
 
-  constructor(){
+  constructor(private productsService:ProductsService){
     for(var card of mock_card_list){
       console.log(card);
       this.cards.push(card);
     }
   }
   ngOnInit(): void {
+    this.productsService.getProducts().subscribe((data: ProductModel []) => {
+      console.log("Fetching Products");
+      console.log(data);
+    });
     throw new Error("Method not implemented");
   }
 }
