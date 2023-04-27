f38eb6d4e691abb41d73b1267a5cf918945605c2
MelonCaully <maloneingham@gmail.com>
2023-04-27 09:01:57 -0400

    Lab 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductModel } from './vertical-card/product.model';
 import { mock_card_list } from './vertical-card/mock_card_list';
+import { ProductsService } from './vertical-card/products.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,10 +11,16 @@ export class AppComponent {
   title = 'snapchat';
   products: ProductModel [] = [];
 
-  constructor(){
-    //for(var product of mock_card_list){
-      //console.log(product);
-      //this.products.push(product);
-    //}
+  constructor(private Productservice:ProductsService){
+
+  }
+
+  ngOnInit(): void {
+    this.Productservice.getProducts().subscribe((data: ProductModel []) =>{
+      for (var product of data){
+        console.log(product)
+        this.products.push(product)
+      }
+    })
   }
 }
