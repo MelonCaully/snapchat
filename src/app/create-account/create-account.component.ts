a377be2e1bb0f46b4a9db88a51b7ba9c17887694
MelonCaully <maloneingham@gmail.com>
2023-05-03 18:21:17 -0400

    Checkpoint

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-create-account',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent {
+  constructor(private afAuth: AngularFireAuth, private database: AngularFireDatabase, private router: Router) {}
 
+  async createAccount(
+    email: string,
+    username: string,
+    password: string,
+    confirmPassword: string,
+  ) {
+    if (password !== confirmPassword) {
+      alert("Password does not match.");
+      return;
+    }
+
+    try{
+      const result = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      if(result.user) {
+        await result.user.updateProfile({ displayName: username});
+
+        await this.database.collection('users').doc(result.user.uid).set({
+          email: email,
+          username: username,
+        });
+      }
+      this.router.navigate(['']);
+    } catch (error: any) {
+      alert('Error: ${error.message')
+    }
+  }
+    
 }
